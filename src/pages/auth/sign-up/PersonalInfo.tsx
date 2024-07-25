67732cfab99be3f53c17406fac818387568ae13a
revelation <nusktecsoft@gmail.com>
2024-07-25 15:34:51 +0100

    Auth - Privacy Link Added

--- src/pages/auth/sign-up/PersonalInfo.tsx
+++ src/pages/auth/sign-up/PersonalInfo.tsx
@@ -178,7 +178,7 @@ export default function PersonalInfo({ props, setActiveTab }: AuthProps) {
                 <div className={` flex items-center gap-3`}>
                     <input type="checkbox" required className='accent-ryd-primary hover:cursor-pointer' />
                     <label className={labelStyle}>
-                        I agree to the <a href='' target='_blank' className='text-ryd-primary'>terms</a> and <a href='' target='_blank' className='text-ryd-primary'>conditions</a>
+                        I agree to the <a href='/privacy.pdf' target='_blank' className='text-ryd-primary'>terms</a> and <a href='/privacy.pdf' target='_blank' className='text-ryd-primary'>conditions</a>
                     </label>
                 </div>
 
